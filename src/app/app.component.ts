e105ed3ffb35e8cbab8e9df3e1a49069bc140747
pcasano <pc.learnings@pm.me>
2025-02-15 22:20:30 +0100

    form control added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormularComponent } from "./formular/formular.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, FormularComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
---
+++ src/app/formular/formular.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formular',
+  imports: [],
+  templateUrl: './formular.component.html',
+  styleUrl: './formular.component.scss'
+})
+export class FormularComponent {
+
+}
