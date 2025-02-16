58f850a2be5b82ba9077293e4620a7a98d957cd7
pcasano <pc.learnings@pm.me>
2025-02-16 16:32:40 +0100

    icons added to the cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormularComponent } from "./formular/formular.component";
+import { CardsComponent } from "./cards/cards.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, FormularComponent],
+    imports: [RouterOutlet, FormularComponent, CardsComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
--- src/app/formular/formular.component.ts
+++ src/app/formular/formular.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formular',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formular.component.html',
   styleUrl: './formular.component.scss'
 })
 export class FormularComponent {
+
+
+  userForm: FormGroup;
+
+  constructor() {
+    this.userForm = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName1: new FormControl('', [Validators.required]),
+      lastName2: new FormControl('', [Validators.required])
+    });
+  }
+
+  onNext() {
+    console.log(this.userForm);
+    }
 
 }
