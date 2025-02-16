b1d83aea64f761988c4f00a168e7cf3fff0829cc
pcasano <pc.learnings@pm.me>
2025-02-16 20:40:29 +0100

    added floating bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormularComponent } from "./formular/formular.component";
 import { CardsComponent } from "./cards/cards.component";
+import { FloatingBarComponent } from "./floating-bar/floating-bar.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, FormularComponent, CardsComponent],
+    imports: [RouterOutlet, FormularComponent, CardsComponent, FloatingBarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
---
+++ src/app/floating-bar/floating-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floating-bar',
+  imports: [],
+  templateUrl: './floating-bar.component.html',
+  styleUrl: './floating-bar.component.scss'
+})
+export class FloatingBarComponent {
+onNext() {
+throw new Error('Method not implemented.');
+}
+
+}
